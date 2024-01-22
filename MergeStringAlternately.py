d8adc853e363b6f015ce941d0bcba06c06073154
Amisha Naik <132745732+amishana1k@users.noreply.github.com>
2024-01-22 23:17:56 +0530

    MergeStringAlternately.py

---
+++ MergeStringAlternately.py
@@ -0,0 +1,11 @@
+class Solution:
+    def mergeAlternately(self, word1: str, word2: str) -> str:
+        i, j = 0, 0
+        s = ""
+        
+        while i < len(word1) and j < len(word2):
+            s += word1[i] + word2[j]
+            i += 1
+            j += 1
+        s += word1[i:] + word2[j:]
+        return s
