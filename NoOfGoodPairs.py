65dcd0873bf0bb767f352ece4a0dc2af0cb0a92b
Amisha Naik <132745732+amishana1k@users.noreply.github.com>
2024-02-29 23:59:31 +0530

    NoOfGoodPairs.py

---
+++ NoOfGoodPairs.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+    def numIdenticalPairs(self, nums: List[int]) -> int:
+        c = 0
+        n = len(nums)
+        for i in range(n):
+            for j in range(i + 1, n):
+                if nums[i] == nums[j]:
+                    c += 1
+        return c
